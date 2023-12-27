f917c609a54553041f86416ef242a00993ce50f5
Harsh <153886419+Harsh-nodejs@users.noreply.github.com>
2023-12-27 17:44:12 +0530

    update

--- controller/pokeController.js
+++ controller/pokeController.js
@@ -43,17 +43,59 @@ exports.getGeneration = async (req, res) => {
 
 exports.getOne = async (req, res, next) => {
   // check ID for number or Name
-  const param = req.params.Id;
+  let param = req.params.Id;
   let queryst = '';
+
+  if (param === 'randomPokemon') {
+    param = Math.floor(Math.random() * 898);
+  } // Checking for Random Pokemon
+
   if (!isNaN(param)) {
     queryst = `{ "pokedex_number" : ${param} }`;
   } else {
     queryst = `{ "pokemon_name" : "${param}" }`;
   }
-  queryst = JSON.parse(queryst);
 
+  queryst = JSON.parse(queryst);
+  // console.log(queryst);
   // Querying
   const features = new APIFeatures(Pokedex.find(queryst), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const pokedex = await features.query;
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: pokedex.length,
+    data: {
+      pokedex,
+    },
+  });
+};
+
+exports.getLegendary = async (req, res) => {
+  const features = new APIFeatures(Pokedex.find({ legendary: true }), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const pokedex = await features.query;
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: pokedex.length,
+    data: {
+      pokedex,
+    },
+  });
+};
+
+exports.getMythical = async (req, res) => {
+  const features = new APIFeatures(Pokedex.find({ mythical : true }), req.query)
     .filter()
     .sort()
     .limitFields()
--- routes/pokeRoutes.js
+++ routes/pokeRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const pokeController = require('../controller/pokeController');
 
-
 const router = express.Router();
 
 router.route('/').get(pokeController.getAll);
 
-router
-    .route('/generation/:genId')
-    .get(pokeController.getGeneration)
+router.route('/generation/:genId').get(pokeController.getGeneration);
 
-router
-    .route('/:Id')
-    .get(pokeController.getOne)
+router.route('/:Id').get(pokeController.getOne);
+
+router.route('/special/legendary').get(pokeController.getLegendary);
+
+router.route('/special/mythical').get(pokeController.getMythical);
 
 module.exports = router;
